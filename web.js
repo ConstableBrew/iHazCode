bce7f73680137d6acf404c279ed5124e10f34d4b
Michael Brewer <constablebrew@gmail.com>
2013-11-24 17:36:20 -0800

    HTML Prototype

--- web.js
+++ web.js
@@ -5,17 +5,17 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
 	//response.send(200);
-	response.send('a'+__dirname+'z');
-	//response.sendfile('viewModels/Portfolio.html', {root: './public'});
+	//response.send('a'+__dirname+'z');
+	response.sendfile(__dirname + '/viewModels/Test.html');
 });
-
+/*
 app.get('/:path/:file', function(request, response){
   var path = request.params.path,
 	file = request.params.file;
   
 	response.sendfile(path + '/' + file, {root: './public'});
 });
-
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
