923f0509aac4b1e5d58eb3e5f3260f914e41ffd6
Michael Brewer <constablebrew@gmail.com>
2013-11-24 22:07:25 -0800

    HTML Prototype

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+var ONE_DAY = 0;//86400000;
+
+app.use(express.static(__dirname + '/public', { maxAge: ONE_DAY }));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
