366bb3af3052bed5cb0fd6ac1fc69661694ba012
Michael Brewer <constablebrew@gmail.com>
2014-10-09 06:14:10 -0700

    Change to MVC style

--- app/config/server.js
+++
@@ -1,7 +0,0 @@
-// Server app configuration ===================================================
-module.exports = function(app, express){
-	app.use(express.static(__dirname + '/public'));	// Set static file location
-	app.use(express.logger('dev'));					// Log every request to the console
-	app.use(express.bodyParser());					// Pull information from html in POST
-	app.use(express.methodOverride());				// Simulate DELETE and PUT
-};
